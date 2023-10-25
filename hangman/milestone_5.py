9b0a762547be394139b6d350bbc80ce3b246c103
Misha McFeat <misha.mcfeat@gmail.com>
2023-10-25 13:28:58 +0100

    Changes to the docstring of the project

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -1,6 +1,42 @@
 from random import choice
 
 class Hangman:
+    '''
+    A Hangman Game that asks the user for a letter and checks if it is in the word.
+    It starts with a default number of lives and a random word from the word_list.
+
+    
+    Parameters:
+    ----------
+    word_list: list
+        List of words to be used in the game
+    num_lives: int
+        Number of lives the player has
+    
+    Attributes:
+    ----------
+    word: str
+        The word to be guessed, picked randomly from the word_list (that contains 5 fruits)
+    word_guessed: list
+        A list of the letters of the word, with '_' for each letter not yet guessed
+        For example, if the word is 'lychee', the word_guessed list would be ['_', '_', '_', '_', '_', '_']
+        If the player guesses 'e', the list would be ['_', '_', '_', '_', 'e', 'e']
+    num_letters: int
+        The number of UNIQUE letters in the word that have not been guessed yet
+    num_lives: int
+        The number of lives the player has
+    list_letters: list
+        A list of the letters that have already been tried
+
+    Methods:
+    -------
+    check_letter(guess)
+        Checks if the letter is in the word and then updates the game state variables accordingly.
+    ask_for_input()
+        Asks the user to guess a letter and handles the input validation.
+    '''
+    
+    
     def __init__(self, word_list, num_lives=5):
         self.word = choice(word_list)
         self.word_guessed = ['_' for _ in self.word]
@@ -8,6 +44,9 @@ class Hangman:
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
+        
+        print(f"The mistery word has {self.num_letters} characters")
+        print(' '.join(self.word_guessed))
     
     def check_guess(self, guess):
         guess = guess.lower()
@@ -35,17 +74,12 @@ class Hangman:
                 break
 
 def play_game(word_list):
-    num_lives = 5
-    game = Hangman(word_list, num_lives)
-    
-    while True:
-        if game.num_lives == 0:
-            print('You lost!')
-            break
-        elif game.num_letters > 0:
-            game.ask_for_input()
-        else:
-            print('Congratulations. You won the game!')
-            break
+    game = Hangman(word_list, num_lives=5)
+    while game.num_lives > 0 and game.num_letters > 0:
+        game.ask_for_input()
+    if game.num_letters == 0:
+        print("Congratulations! You won!")
+    else:
+        print(f"You lost! The word was {game.word}")
 
 play_game(['orange', 'lychee', 'grapes', 'kiwi', 'banana'])
