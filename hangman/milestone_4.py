3ca6051cc59f9426f784907d895c09e689f04479
Misha McFeat <misha.mcfeat@gmail.com>
2023-10-25 12:52:26 +0100

    Changes structure to fit requirements

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,45 +1,39 @@
 from random import choice
 
-class Hangman():
+class Hangman:
+    
     def __init__(self, word_list, num_lives=5):
         self.word = choice(word_list)
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
-        self.word_guessed = ['_' for letter in self.word]
-        self.num_letters = list(set([letter for letter in self.word if letter not in self.list_of_guesses]))
-        return None
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
     
     def check_guess(self, guess):
-        self.guess = guess
-        if self.guess.lower() in self.word:
-            print(f"Good guess! {self.guess} is in the word {self.word}.")
-            print([letter if letter in self.list_of_guesses else '_' for letter in self.word])
+        if guess.lower() in self.word:
+            print(f"Good guess! {guess} is in the word {self.word}.")
             return True
         else:
             self.num_lives -= 1
-            print(f"Sorry, {self.guess} is not in the word. Try again.")
-            print(f"ou have {self.num_lives} lives left.")
+            print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
             return False
             
     def ask_for_input(self):
         while True:
-            self.guess = input("Guess a letter please: ")
-            if len(self.guess) != 1 or not self.guess.isalpha():
+            guess = input("Guess a letter please: ")
+            if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif self.guess in self.list_of_guesses:
+            elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
-                if self.check_guess(self.guess):
-                    self.list_of_guesses.append(self.guess)
-                    break
+                if self.check_guess(guess):
+                    self.list_of_guesses.append(guess)
+                    self.word_guessed = [letter if letter in self.list_of_guesses else '_' for letter in self.word]
+                else:
+                    self.list_of_guesses.append(guess)  # We should also add incorrect guesses to the list
                 break
-                
-    def current_blanks(self):
-        return 
-         
-    def unique_letters(self):
-        return 
 
     
     
